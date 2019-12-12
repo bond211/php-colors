a67cfb4ef753d080baf7de0db63e5983d31a6635
Alexander Bondar <alexander@bondar.de>
2019-12-12 10:09:24 +0100

    Add HSL to RGB converter

--- src/Converters/HslToRgb.php
+++ src/Converters/HslToRgb.php
@@ -6,6 +6,50 @@ class HslToRgb
 {
     public static function toArray(int $h, int $s, int $l): array
     {
+        $h /= 360;
+        $s /= 100;
+        $l /= 100;
+
+        $r = $g = $b = $l;
+
+        if ($s > 0) {
+            $q = $l < 0.5
+                ? $l * (1 + $s)
+                : $l + $s - $l * $s;
+
+            $p = 2 * $l - $q;
+
+            $r = self::hue2rgb($p, $q, $h + 1 / 3);
+            $g = self::hue2rgb($p, $q, $h);
+            $b = self::hue2rgb($p, $q, $h - 1 / 3);
+        }
+
+        $r = round($r * 255);
+        $g = round($g * 255);
+        $b = round($b * 255);
+
         return [$r, $g, $b];
+    }
+
+    private static function hue2rgb($p, $q, $t)
+    {
+        if ($t < 0) {
+            $t += 1;
+        }
+        if ($t > 1) {
+            $t -= 1;
+        }
+
+        if ($t < 1 / 6) {
+            return $p + ($q - $p) * 6 * $t;
+        }
+        if ($t < 1 / 2) {
+            return $q;
+        }
+        if ($t < 2 / 3) {
+            return $p + ($q - $p) * (2 / 3 - $t) * 6;
+        }
+
+        return $p;
     }
 }

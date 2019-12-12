324c6f2e78aac5558dd03ca37343ffd8d17191ad
Alexander Bondar <alexander@bondar.de>
2019-12-12 08:19:48 +0100

    Add CMYK to RGB converter

--- src/Converters/CmykToRgb.php
+++ src/Converters/CmykToRgb.php
@@ -6,6 +6,14 @@ class CmykToRgb
 {
     public static function toArray(int $c, int $m, int $y, int $k): array
     {
+        $r = (100 - $c) * (100 - $k) * .0255;
+        $g = (100 - $m) * (100 - $k) * .0255;
+        $b = (100 - $y) * (100 - $k) * .0255;
+
+        $r = floor($r);
+        $g = floor($g);
+        $b = floor($b);
+
         return [$r, $g, $b];
     }
 }

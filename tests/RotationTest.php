597615458f858ff0c476dd684e1e78218e30b07e
Alexander Bondar <alexander@bondar.de>
2019-12-12 14:28:12 +0100

    Add rotation test

---
+++ tests/RotationTest.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests;
+
+use Bond211\Colors\Color;
+use PHPUnit\Framework\TestCase;
+
+final class RotationTest extends TestCase
+{
+    public function test1(): void
+    {
+        $color = Color::fromHsl(242, 80, 73);
+
+        $rotated10 = $color->rotate(10);
+        $rotated20 = $color->rotate(20);
+
+        $this->assertEquals(252, $rotated10->hsl->h);
+        $this->assertEquals(262, $rotated20->hsl->h);
+    }
+}

42491ddbc8cd288a38df7a29aba3e6fcc491db3b
Alexander Bondar <alexander@bondar.de>
2019-12-12 09:57:14 +0100

    Add HSV to RGB converter

--- src/Converters/HsvToRgb.php
+++ src/Converters/HsvToRgb.php
@@ -6,6 +6,41 @@ class HsvToRgb
 {
     public static function toArray(int $h, int $s, int $v): array
     {
-        return [$r, $g, $b];
+        $rgb = self::toBaseRgb($h);
+        $rgb = self::toActualRgb($s, $v, $rgb);
+
+        return $rgb;
+    }
+
+    private static function toBaseRgb(int $h): array
+    {
+        $rgb = [0, 0, 0];
+
+        for ($i = 0; $i < 3; $i++) {
+            $deg = abs($h - $i * 120);
+
+            if ($deg >= 120) {
+                continue;
+            }
+
+            $distance = max(60, $deg);
+            $rgb[$i % 3] = 1 - (($distance - 60) / 60);
+        }
+
+        return $rgb;
+    }
+
+    private static function toActualRgb(int $s, int $v, array $rgb): array
+    {
+        $max = max($rgb);
+        $factor = 255 * ($v / 100);
+
+        for ($i = 0; $i < 3; $i++) {
+            $d = $max - $rgb[$i];
+            $f = $rgb[$i] + $d * (1 - $s / 100);
+            $rgb[$i] = round($factor * $f);
+        }
+
+        return $rgb;
     }
 }
